87079b638db286d0e75501f84e7107fa9ee48231
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-03-21 20:36:30 +0530

    my commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const add=require("./add");
+const sub=require("./subtract");
+const mul=require("./multiply");
+const div=require("./divide");
+
+let a=5;
+let b=2;
+
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(mul(a,b));
+console.log(div(a,b));
